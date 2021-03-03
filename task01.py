723b2407c89ac7dc0bfebbcba83bbcf6ccb270c3
Ilia-Pashkin <ilya.pashkin239@gmail.com>
2021-03-03 15:35:07 +0300

    Задания по курсу python

--- task01.py
+++
@@ -1,7 +0,0 @@
-with open("file01.txt", "w") as text_file:
-    while True:
-        line = input()
-        if line != "":
-            text_file.write(line + '\n')
-        else:
-            break
